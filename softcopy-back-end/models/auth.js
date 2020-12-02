543c6fd48f26bf3262c08d3cc3cb50feef64a855
Lucky0108 <raolakshayyadav02@gmail.com>
2020-12-02 23:18:43 +0530

    Added Models and route for singup api

---
+++ softcopy-back-end/models/auth.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 20
+    },
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        index: true,
+        min: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    hash_password: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: String
+    }
+},{timestamps: true});
+
+UserSchema.virtual('password')
+.set((password) => {
+    this.hash_password = bcrypt.hashSync(password, 10);
+});
+
+UserSchema.virtual('fullName')
+.set(() => {
+    return `${this.firstName} ${this.lastName}`
+})
+
+UserSchema.methods = {
+    authenticate: (password) => {
+        return bcrypt.compareSync(password, this.hash_password);
+    }
+}
+
+module.exports = mongoose.model("User", UserSchema)
--- softcopy-back-end/server.js
+++ softcopy-back-end/server.js
@@ -3,8 +3,13 @@ const app = express();
 const env = require('dotenv');
 const mongoose = require('mongoose')
 
+// Routes
+const authRoutes = require('./routes/auth')
+
+// Environment Variable Config
 env.config()
 
+// Database Connect
 mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.yy8pj.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
 {
     useNewUrlParser: true, 
@@ -16,7 +21,10 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO
             console.log("Database Connected !!!")
         })
 
+
+// Route Setup
 app.use(express.json())
+app.use('/api', authRoutes)
 
 app.listen(4000,() => {
     console.log(`Server is running on port 4000`)
