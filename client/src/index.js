e9a0b4ca0b9646d38b1c85a91c0ef32cf8db0495
Lucky0108 <raolakshayyadav02@gmail.com>
2020-12-31 22:19:50 +0530

    Added perfect countdown

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import Countdown from './Containers/Countdown';
 window.store = store;
 
 ReactDOM.render(
   <>
     <Provider store={store}>
     <BrowserRouter>
-    <App />
+    {/* <App /> */}
+    <Countdown />
     </BrowserRouter>
     </Provider>
   </>,
