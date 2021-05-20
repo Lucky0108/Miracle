97bf16ccdbb9cee32655402ac78d5fe70ad23324
Lucky0108 <raolakshayyadav02@gmail.com>
2021-05-20 16:53:24 +0530

    removing npm version to check build

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { getCategoryById } = require('../controllers/category')
+const { getUserById } = require('../controllers/user')
+const { getBlogById } = require('../controllers/blog')
 
-
+// Params
+// router.param("blogId", getBlogById)
+router.param("categoryId", getCategoryById);
+router.param("userId", getUserById);
 
 module.exports = router;
