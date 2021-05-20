2b0013d6a7d95abb7b138f2380b04f3f739385db
Lucky0108 <raolakshayyadav02@gmail.com>
2021-05-20 17:10:07 +0530

    corrected query payload condition

--- client/src/actions/query.action.js
+++ client/src/actions/query.action.js
@@ -17,7 +17,7 @@ export const queryAction = (details) => {
 
         res.catch((error) => {
             if(error.response) {
-                if(error.response.status === 400) {
+                if(error.response.status === 400 || error.response.status === 422) {
                     dispatch({ type: queryConstants.QUERY_FAILURE,
                         payload: { message: error.response.data.message || error.response.data.error }
                     })
